4cd382c3b7f70232c93a59a5d73d96b1ec63f352
Daniel Goerz <daniel.goerz@b13.de>
2018-10-15 09:47:32 +0200

    [TASK] Raise compat versions for development

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
   'author_company' => 'Localization Manager Team',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '8.7.0-8.99.99',
+      'typo3' => '9.4.0',
       'static_info_tables' => '6.4.2-0.0.0'
     ),
     'conflicts' => array(),
