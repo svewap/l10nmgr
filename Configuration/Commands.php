f15c8c302479dfcf4500094cac7fc37f00beaea6
micgiek1 <michael.giek@web.de>
2018-10-15 15:35:05 +0200

    [TASK] Introduce basic symfony command structure
    * [TASK] Change folder structure and add the CLI config
    
    * [TASK] Set the Export Command with all options
    
    * [TASK] Set the Export Command with all options
    After code style fixing
    
    * [TASK] Change misspelling
    
    * [TASK] Set the Import Command with all options
    
    * [TASK] Delete README
    
    * [TASK] Add the old code for reminding

---
+++ Configuration/Commands.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'l10nmanager:export' => [
+        'class' => \Localizationteam\L10nmgr\Command\Export::class,
+    ],
+    'l10nmanager:import' => [
+        'class' => \Localizationteam\L10nmgr\Command\Import::class,
+    ],
+];
