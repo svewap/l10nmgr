de3d720e8dc2612dff9847a9386ef90d59c380fe
Cybercraft <info@cybercraft.de>
2019-08-12 18:06:23 +0200

    [TASK] Cleanup and fix tasks for scheduler, export and import
    Change-Id: I4cc34ea1cb47f806ceb7d5ab6ff19a6772b677eb

--- Classes/Task/L10nmgrFileGarbageCollection.php
+++ Classes/Task/L10nmgrFileGarbageCollection.php
@@ -25,7 +25,6 @@ use Exception;
 use RuntimeException;
 use SplFileInfo;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Scheduler\Task\AbstractTask;
 
 /**
  * L10N Manager file garbage collection task
