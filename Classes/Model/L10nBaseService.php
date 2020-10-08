24f5f94467091404411c03014ff55296c6ddff03
cybercraft <info@cybercraft.de>
2020-10-08 13:58:45 +0200

    [BUGFIX] Make sure containers with language "all" won't get triggered by child translation

--- Classes/Model/L10nBaseService.php
+++ Classes/Model/L10nBaseService.php
@@ -599,9 +599,10 @@ class L10nBaseService implements LoggerAwareInterface
                                             if ((int)$element['colPos'] !== -2 && (int)$element['colPos'] !== -1 && (int)$element['colPos'] !== 18181) {
                                                 $this->TCEmain_cmd['tt_content'][$elementUid]['localize'] = $Tlang;
                                             } else {
+                                                $translateThroughParent = false;
                                                 if ($element['tx_gridelements_container'] > 0) {
                                                     $this->depthCounter = 0;
-                                                    $this->recursivelyCheckForRelationParents(
+                                                    $translateThroughParent = $this->recursivelyCheckForRelationParents(
                                                         $element,
                                                         $Tlang,
                                                         'tx_gridelements_container',
@@ -610,12 +611,15 @@ class L10nBaseService implements LoggerAwareInterface
                                                 }
                                                 if ($element['tx_flux_parent'] > 0) {
                                                     $this->depthCounter = 0;
-                                                    $this->recursivelyCheckForRelationParents(
+                                                    $translateThroughParent = $this->recursivelyCheckForRelationParents(
                                                         $element,
                                                         $Tlang,
                                                         'tx_flux_parent',
                                                         'tx_flux_children'
                                                     );
+                                                }
+                                                if (!$translateThroughParent) {
+                                                    $this->TCEmain_cmd['tt_content'][$elementUid]['localize'] = $Tlang;
                                                 }
                                             }
                                         } elseif ($table === 'sys_file_reference') {
@@ -906,10 +910,16 @@ class L10nBaseService implements LoggerAwareInterface
     }
 
     /**
+    /**
+     * Issues 'localize' to the DataHandler cmd map for parents of the given element recursively.
+     *
+     * Returns true if the parent will be localized, otherwise false.
+     *
      * @param $element
      * @param $Tlang
      * @param $parentField
      * @param $childrenField
+     * @return bool
      */
     protected function recursivelyCheckForRelationParents($element, $Tlang, $parentField, $childrenField)
     {
@@ -940,12 +950,18 @@ class L10nBaseService implements LoggerAwareInterface
             }
             if ($translatedParent['uid'] > 0) {
                 $this->TCEmain_cmd['tt_content'][$translatedParent['uid']]['inlineLocalizeSynchronize'] = $childrenField . ',localize';
+                return true;
             } else {
                 if ($element[$parentField] > 0) {
                     $parent = $this->getRawRecord('tt_content', (int)$element[$parentField]);
-                    $this->recursivelyCheckForRelationParents($parent, $Tlang, $parentField, $childrenField);
+                    return $this->recursivelyCheckForRelationParents($parent, $Tlang, $parentField, $childrenField);
                 } else {
-                    $this->TCEmain_cmd['tt_content'][$element['uid']]['localize'] = $Tlang;
+                    if ($element['sys_language_uid'] !== -1) {
+                        $this->TCEmain_cmd['tt_content'][$element['uid']]['localize'] = $Tlang;
+                        return true;
+                    } else {
+                        return false;
+                    }
                 }
             }
         }
