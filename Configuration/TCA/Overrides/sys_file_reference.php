22e9a4c1bdb44219cb41c82c4492ed08fff92832
André Schließer <andy.schliesser@gmail.com>
2020-11-02 11:28:44 +0000

    [BUGFIX] Override only type of sys_file_refence columns
    (cherry picked from commit 991a7f601adfa711581f6e1572dff81bcc6d5c32)

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -1,5 +1,5 @@
 <?php
 
-$GLOBALS['TCA']['sys_file_reference']['columns']['tablenames']['config'] = ['type' => 'passthrough'];
-$GLOBALS['TCA']['sys_file_reference']['columns']['fieldname']['config'] = ['type' => 'passthrough'];
-$GLOBALS['TCA']['sys_file_reference']['columns']['table_local']['config'] = ['type' => 'passthrough'];
+$GLOBALS['TCA']['sys_file_reference']['columns']['tablenames']['config']['type'] = 'passthrough';
+$GLOBALS['TCA']['sys_file_reference']['columns']['fieldname']['config']['type'] = 'passthrough';
+$GLOBALS['TCA']['sys_file_reference']['columns']['table_local']['config']['type'] = 'passthrough';
