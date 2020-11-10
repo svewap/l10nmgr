36c9b32cc8d8caaee312143ccbe507a77cf5e912
cybercraft <info@cybercraft.de>
2020-11-10 13:27:53 +0000

    [TASK] Remove deprecated configuration parameter completely
    (cherry picked from commit 108bd26b0113c8c777b14043bb5940d06a58695f)

--- Classes/Model/Tools/Tools.php
+++ Classes/Model/Tools/Tools.php
@@ -367,20 +367,8 @@ class Tools
                 if (
                     isset($GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'])
                     && isset($GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'][$field])
-                    && (
-                        (
-                            isset($GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'][$field]['config']['defaultExtras'])
-                            && strpos(
-                                $GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'][$field]['config']['defaultExtras'],
-                                'richtext'
-                            ) !== false
-                        )
-                        ||
-                        (
-                            isset($GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'][$field]['config']['enableRichtext'])
-                            && $GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'][$field]['config']['enableRichtext']
-                        )
-                    )
+                    && isset($GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'][$field]['config']['enableRichtext'])
+                    && $GLOBALS['TCA'][$table]['types'][$TCAtype]['columnsOverrides'][$field]['config']['enableRichtext']
                 ) {
                     $isRTE = true;
                 } else {
